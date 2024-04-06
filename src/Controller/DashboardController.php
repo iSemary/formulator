bf04e5cc2ffbc46fb9890ef41bce0f612d9d7d0b
Abdelrahman Mostafa <abdelrahmandeveloper123@gmail.com>
2024-04-06 23:27:03 +0200

    Added form counter

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use App\Service\FormService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController {
+    private $formService;
+    public function __construct(FormService $formService) {
+        $this->formService = $formService;
+    }
+
+
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response {
+        $totalForms = $this->formService->getTotalForms();
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'totalForms' => $totalForms,
+            'leads' => 'DashboardController',
         ]);
     }
 }
--- src/Repository/FormRepository.php
+++ src/Repository/FormRepository.php
@@ -14,35 +14,35 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Form[]    findAll()
  * @method Form[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class FormRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
+class FormRepository extends ServiceEntityRepository {
+    public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, Form::class);
     }
 
-//    /**
-//     * @return Form[] Returns an array of Form objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('f')
-//            ->andWhere('f.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('f.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * @return Form[] Returns an array of Form objects
+     */
+    public function findOneByIdAndUserId(int $id, int $userId) {
+        return $this->createQueryBuilder('f')
+            ->andWhere('f.id = :id')
+            ->setParameter('id', $id)
+            ->andWhere('f.userId = :userId')
+            ->setParameter('userId', $userId)
+            ->orderBy('f.id', 'ASC')
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 
-//    public function findOneBySomeField($value): ?Form
-//    {
-//        return $this->createQueryBuilder('f')
-//            ->andWhere('f.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+
+    /**
+     * @return int Returns numeric number of total forms for specific user
+     */
+    public function getTotalForms(int $userId): int {
+        return $this->createQueryBuilder('f')
+            ->select('COUNT(f.id)')
+            ->andWhere('f.userId = :userId')
+            ->setParameter('userId', $userId)
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
 }
